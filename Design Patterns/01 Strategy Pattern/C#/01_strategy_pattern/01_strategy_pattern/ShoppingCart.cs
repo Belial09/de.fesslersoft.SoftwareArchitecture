4d8fc35d468c6abc46c4057c002336c8c414d6a6
M. Feßler <mf@htk.de>
2023-02-06 21:39:07 +0100

    Added the Strategy Pattern C# Sample Solution.

---
+++ Design Patterns/01 Strategy Pattern/C#/01_strategy_pattern/01_strategy_pattern/ShoppingCart.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using _01_strategy_pattern.interfaces;
+
+namespace _01_strategy_pattern
+{
+    internal sealed class ShoppingCart
+    {
+        internal List<Tuple<IItem, IPaymentMethod>> _items;
+
+        public ShoppingCart()
+        {
+            this._items = new List<Tuple<IItem, IPaymentMethod>>();
+        }
+
+        public void AddItem(IItem item, IPaymentMethod paymentMethod)
+        {
+            this._items.Add(new Tuple<IItem, IPaymentMethod>(item, paymentMethod));
+            Console.WriteLine($"Added Item '{item.Name}' with PaymentMethod {paymentMethod.getPaymentMethodName()} into ShoppingCart.");
+        }
+
+        public Tuple<IItem, IPaymentMethod> getItem(Guid itemIdentifier)
+        {
+            return _items.FirstOrDefault(i => i.Item1.Identifier == itemIdentifier);
+        }
+
+        public void removeItem(Tuple<IItem, IPaymentMethod> itemTuple)
+        {
+            if (itemTuple != null)
+            {
+                _items.Remove(itemTuple);
+                Console.WriteLine($"Removed Item '{itemTuple.Item1.Name}' from ShoppingCart.");
+            }
+            else
+            {
+                Console.WriteLine($"Item 'UNKNOWN not found in ShoppingCart.");
+            }
+        }
+
+        public void Checkout()
+        {
+            foreach (var item in _items)
+            {
+                Console.WriteLine($"Item {item.Item1.Name} will be payed using {item.Item2.getPaymentMethodName()}");
+            }
+        }   
+    }
+}
---
+++ Design Patterns/01 Strategy Pattern/C#/01_strategy_pattern/01_strategy_pattern_tests/Class1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Linq;
+using NUnit.Framework;
+using Moq;
+using _01_strategy_pattern;
+using _01_strategy_pattern.interfaces;
+using _01_strategy_pattern.entities.PaymentMethods;
+using _01_strategy_pattern.entities;
+
+namespace _01_strategy_pattern_tests
+{
+    [TestFixture]
+    public class ShoppingCartTests
+    {
+        private ShoppingCart _shoppingCart;
+        private Mock<IItem> _mockItem;
+        private Mock<IPaymentMethod> _mockPaymentMethod;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _shoppingCart = new ShoppingCart();
+            _mockItem = new Mock<IItem>();
+            _mockPaymentMethod = new Mock<IPaymentMethod>();
+
+            _mockItem.Setup(x => x.Identifier).Returns(Guid.NewGuid());
+            _mockItem.Setup(x => x.Name).Returns("Item 1");
+
+            _mockPaymentMethod.Setup(x => x.getPaymentMethodName()).Returns("Paypal");
+        }
+
+        [Test]
+        public void AddItem_ItemAndPaymentMethod_ItemAddedSuccessfully()
+        {
+            // Act
+            _shoppingCart.AddItem(_mockItem.Object, _mockPaymentMethod.Object);
+
+            // Assert
+            Assert.IsTrue(_shoppingCart._items.Count == 1);
+        }
+
+        [Test]
+        public void GetItem_ValidItemIdentifier_ReturnsMatchingItem()
+        {
+            // Arrange
+            var expectedIdentifier = _mockItem.Object.Identifier;
+            _shoppingCart.AddItem(_mockItem.Object, _mockPaymentMethod.Object);
+
+            // Act
+            var actualItem = _shoppingCart.getItem(expectedIdentifier);
+
+            // Assert
+            Assert.IsNotNull(actualItem);
+            Assert.AreEqual(expectedIdentifier, actualItem.Item1.Identifier);
+        }
+
+        [Test]
+        public void GetItem_InvalidItemIdentifier_ReturnsNull()
+        {
+            // Act
+            var actualItem = _shoppingCart.getItem(Guid.NewGuid());
+
+            // Assert
+            Assert.IsNull(actualItem);
+        }
+
+        [Test]
+        public void RemoveItem_ValidItem_ItemRemovedSuccessfully()
+        {
+            // Arrange
+            var expectedItem = new Tuple<IItem, IPaymentMethod>(_mockItem.Object, _mockPaymentMethod.Object);
+            _shoppingCart.AddItem(_mockItem.Object, _mockPaymentMethod.Object);
+
+            // Act
+            _shoppingCart.removeItem(expectedItem);
+
+            // Assert
+            Assert.IsTrue(_shoppingCart._items.Count == 0);
+        }
+
+        [Test]
+        public void RemoveItem_InvalidItem_NoItemRemoved()
+        {
+            //Arrange
+            var cart = new ShoppingCart();
+            var item = new Item("apple");
+            var paymentMethod = new PayPal();
+            var itemTuple = new Tuple<IItem, IPaymentMethod>(item, paymentMethod);
+
+            //Act
+            cart.removeItem(itemTuple);
+
+            //Assert
+            Assert.That(cart._items.Count, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void TestRemoveItem()
+        {
+            //Arrange
+            var item = new Mock<IItem>();
+            item.SetupGet(i => i.Identifier).Returns(Guid.NewGuid());
+            item.SetupGet(i => i.Name).Returns("item1");
+            var paymentMethod = new Mock<IPaymentMethod>();
+            paymentMethod.Setup(pm => pm.getPaymentMethodName()).Returns("PayPal");
+            var cart = new ShoppingCart();
+            cart.AddItem(item.Object, paymentMethod.Object);
+            cart.removeItem(new Tuple<IItem, IPaymentMethod>(item.Object, paymentMethod.Object));
+            //Assert
+            Assert.IsFalse(cart._items.Any(i => i.Item1.Identifier == item.Object.Identifier));
+        }
+
+        [Test]
+        public void TestCheckout()
+        {
+            //Arrange
+            var item1 = new Mock<IItem>();
+            item1.SetupGet(i => i.Identifier).Returns(Guid.NewGuid());
+            item1.SetupGet(i => i.Name).Returns("item1");
+            var paymentMethod1 = new Mock<IPaymentMethod>();
+            paymentMethod1.Setup(pm => pm.getPaymentMethodName()).Returns("PayPal");
+
+            var item2 = new Mock<IItem>();
+            item2.SetupGet(i => i.Identifier).Returns(Guid.NewGuid());
+            item2.SetupGet(i => i.Name).Returns("item2");
+            var paymentMethod2 = new Mock<IPaymentMethod>();
+            paymentMethod2.Setup(pm => pm.getPaymentMethodName()).Returns("Klarna");
+
+            var cart = new ShoppingCart();
+            cart.AddItem(item1.Object, paymentMethod1.Object);
+            cart.AddItem(item2.Object, paymentMethod2.Object);
+
+            //Act
+            cart.Checkout();
+
+            //Assert
+            Assert.IsTrue(cart._items.Count == 2);
+        }
+    }
+}
