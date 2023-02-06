d0b9ecbb19419dba2a8d696be8c17deaa40823b9
M. Feßler <mf@htk.de>
2023-02-06 21:30:37 +0100

    Added the Strategy Pattern C# Sample Solution.

--- Design Patterns/01 Strategy Pattern/C#/01_strategy_pattern/01_strategy_pattern/Properties/AssemblyInfo.cs
+++ Design Patterns/01 Strategy Pattern/C#/01_strategy_pattern/01_strategy_pattern/Properties/AssemblyInfo.cs
@@ -5,6 +5,9 @@ using System.Runtime.InteropServices;
 // Allgemeine Informationen über eine Assembly werden über die folgenden
 // Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
 // die einer Assembly zugeordnet sind.
+[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
+[assembly: InternalsVisibleTo("DynamicProxyGenAssembly1")]
+[assembly: InternalsVisibleTo("01_strategy_pattern_tests")]
 [assembly: AssemblyTitle("01_strategy_pattern")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
